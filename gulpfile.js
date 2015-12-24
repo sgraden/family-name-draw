2336331486973c44b51f001c20301cfcb35c398d
Steven Raden <stevengraden@gmail.com>
2015-12-24 11:50:17 -0800

    reorganized and got the gulp function working with node and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
-var gulp  = require('gulp');
-var browserify = require('gulp-browserify');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
 
-gulp.task('scripts', function() {
-    // Single entry point to browserify
-    gulp.src('./src/app.js')
-        .pipe(browserify({
-          insertGlobals : true,
-          debug : !gulp.env.production
-        }))
-        .pipe(gulp.dest('./dist/'));
+gulp.task('default', ['browser-sync'], function() {
+
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('./src/*.js', ['scripts']);
+//static server
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:8008",
+		files: ["public/**/*.*"],
+		browser: "google chrome",
+		port: 8000
+	});
+});
+
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	return nodemon({
+		script: 'server.js'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true;
+		}
+	})
 });
